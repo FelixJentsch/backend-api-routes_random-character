f4c09d096acf2c1acd4e7f48c09c5fb9d8061f93
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-19 16:41:46 +0100

    challenge solved

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Error loading data</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Hello from Next.js!</h1>
+      <h2>Random Character</h2>
+      <div>
+        <h3>
+          {data.firstName} {data.lastName}
+        </h3>
+        <p>Age: {data.age}</p>
+        <p>Twitter: {data.twitter}</p>
+        <p>Geohash: {data.geohash}</p>
+      </div>
+    </div>
+  );
+
+  /*  return (
+    <div>
+      <h1>Hello from Next.js!</h1>
+      <h2>Random Character:</h2>                                              // Ugly! 
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  ); */
 }
